1685f21bd81a922ab9564b7ecda4ee1acdc353bd
Dilum Navanjana <dilum@Dilums-MacBook-Pro.local>
2015-03-13 22:02:08 +0530

    changed

--- lib/test2.rb
+++ lib/test2.rb
@@ -2,5 +2,5 @@ require "test2/version"
 
 module Test2
   # Your code goes here...
-  Test2.ui.error "Invalid gem name #{name} Please give a name which does not start with numbers."
+  "Invalid gem name #{name} Please give a name which does not start with numbers."
 end
